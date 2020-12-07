f6e37c22a23a62bf1a6fa1bc03960de4cdb36560
Jerry Robertson <jerry.r.robertson1@gmail.com>
2020-12-07 10:37:55 -0600

    Started into author tags

--- TabloidCLI/UserInterfaceManagers/TagManager.cs
+++ TabloidCLI/UserInterfaceManagers/TagManager.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+using TabloidCLI.Models;
 
 namespace TabloidCLI.UserInterfaceManagers
 {
     public class TagManager : IUserInterfaceManager
     {
         private readonly IUserInterfaceManager _parentUI;
+        private TagRepository _tagRepository;
+        private AuthorRepository _authorRepository;
+        private string _connectionString;
+
 
         public TagManager(IUserInterfaceManager parentUI, string connectionString)
         {
             _parentUI = parentUI;
+            _authorRepository = new AuthorRepository(connectionString);
+            _tagRepository = new TagRepository(connectionString);
+            _connectionString = connectionString;
         }
 
         public IUserInterfaceManager Execute()
@@ -43,7 +52,36 @@ namespace TabloidCLI.UserInterfaceManagers
                     return this;
             }
         }
+        private Author ChooseAuthor(string prompt = null)
+        {
+            if (prompt == null)
+            {
+                prompt = "Please choose an Author:";
+            }
 
+            Console.WriteLine(prompt);
+
+            List<Author> authors = _authorRepository.GetAll();
+
+            for (int i = 0; i < authors.Count; i++)
+            {
+                Author author = authors[i];
+                Console.WriteLine($" {i + 1}) {author.FullName}");
+            }
+            Console.Write("> ");
+
+            string input = Console.ReadLine();
+            try
+            {
+                int choice = int.Parse(input);
+                return authors[choice - 1];
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Invalid Selection");
+                return null;
+            }
+        }
         private void List()
         {
             throw new NotImplementedException();
